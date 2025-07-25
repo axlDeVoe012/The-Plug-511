b3038e87a389cd64b390633cdbd888f2836d5a22
axlDeVoe012 <222691133@tut4life.ac.za>
2025-07-25 23:42:18 +0200

    Deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import './styles/darkMode.css';
+
 
 
 import Home from './pages/Home';
